04e00c242eda09b012239c69d3c565cbc9c34e54
henok2019 <45355566+henok2019@users.noreply.github.com>
2021-12-12 20:45:52 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
+import { PropertyCardCompenent } from './properties/property-card/property-card.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './properties/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardCompenent,
+    PropertyListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
